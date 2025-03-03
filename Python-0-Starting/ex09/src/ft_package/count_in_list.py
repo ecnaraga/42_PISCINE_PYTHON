a1d09e0132da40b845470cbef5c0e276ae1f4d4e
Garance LAMBEY <galambey@bess-f3r5s12.clusters.42paris.fr>
2025-03-03 11:02:10 +0100

    ex09 done

--- Python-0-Starting/ex09/src/ft_package/count_in_list.py
+++ Python-0-Starting/ex09/src/ft_package/count_in_list.py
@@ -1,3 +1,10 @@
-def count_in_list() {
-    print("It's working")
-}
+def count_in_list(args: list, s: any) -> int:
+    '''
+    Return the numbers of occurrences of s in args
+    '''
+    i = 0
+
+    for arg in args:
+        if arg == s:
+            i += 1
+    return i
