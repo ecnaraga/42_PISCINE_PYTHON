0de032ae3e14c1270e6513f936ff56ec23d97429
Garance LAMBEY <galambey@bess-f3r1s12.clusters.42paris.fr>
2025-03-05 17:41:46 +0100

    ex03 corr et ok

--- Python-1-Array/ex03/zoom.py
+++ Python-1-Array/ex03/zoom.py
@@ -7,12 +7,20 @@ def main():
     '''
     '''
     array = ft_load("images/animal.jpeg")
-    print(array)
-    array = array[100:500, 462:862, 0:1]  # Slice image from row 100 to 500, from col 462 to 862, keep first byte for the color 
-    print("New shape after slicing:", np.shape(array))
-    print(array)
-    plt.imshow(array, cmap='gray')  # 
-    plt.show()
+    if array is None:
+        return
+    try:
+        print(array)
+        # Slice image from row 100 to 500, from col 462 to 862, keep 1st byte
+        # for the shape of gray:
+        array = array[100:500, 462:862, 0:1]
+        print("New shape after slicing:", np.shape(array))
+        print(array)
+        # Display data as an image 2D + set colormapping with cmap, vmin, vmax:
+        plt.imshow(array, cmap='gray', vmin=0, vmax=255)
+        plt.show()  # Display all open figure
+    except Exception as e:
+        print("Error :", e)
 
 
 if __name__ == "__main__":
